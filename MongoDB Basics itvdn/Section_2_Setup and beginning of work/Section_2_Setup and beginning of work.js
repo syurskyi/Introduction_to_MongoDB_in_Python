feeed9c5b1a1ee486355a474dec2208e9ea82124
Grey <sergejyurskyj@yahoo.com>
2019-11-04 19:53:30 -0800

    Commit Section_2_Setup and beginning of work

---
+++ MongoDB Basics itvdn/Section_2_Setup and beginning of work/Section_2_Setup and beginning of work.js
@@ -0,0 +1,22 @@
+// use testDb
+//
+// show dbs
+//
+// db
+//
+// db.stats()
+//
+// db.users.insert({name: 'John', age:25})
+//
+// db.stats()
+//
+// db.dropDatabase()
+//
+// mongostat()
+//
+// mongotop()
+//
+// mongoexport --db testDB -- collection users --out C:\users.json
+//
+//
+// mongoimport --db testDB --collection users --file C:\users.json
